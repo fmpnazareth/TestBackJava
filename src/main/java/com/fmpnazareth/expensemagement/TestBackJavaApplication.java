0e4b50c9eb493f6d4c9f8f7a5446f9bf68512f03
Fernando Nazareth <fmpnazareth@gmail.com>
2019-01-12 14:25:05 -0200

    Create spring boot structure for project.

---
+++ src/main/java/com/fmpnazareth/expensemagement/TestBackJavaApplication.java
@@ -0,0 +1,14 @@
+package com.fmpnazareth.expensemagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestBackJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestBackJavaApplication.class, args);
+	}
+
+}
+
