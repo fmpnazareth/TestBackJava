1f61d9f3a76c874bffe54b217d14db824492dc88
Fernando Nazareth <fmpnazareth@gmail.com>
2019-01-13 13:33:24 -0200

    Fix JUnit test.

--- src/test/java/com/fmpnazareth/expensemagement/TestBackJavaApplicationTests.java
+++ src/test/java/com/fmpnazareth/expensemagement/TestBackJavaApplicationTests.java
@@ -6,8 +6,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.fmpnazareth.expensemagement.config.H2TestProfileJPAConfig;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = {
+		  H2TestProfileJPAConfig.class})
 @ActiveProfiles("test")
 public class TestBackJavaApplicationTests {
 
